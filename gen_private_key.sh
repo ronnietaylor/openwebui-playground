fa139d87d3ebac3b7c612a0a168db2fa04bf0247
Beastart <beastart@local>
2026-04-13 01:50:02 -0700

    Save certs relative to repo root.

--- gen_private_key.sh
+++ gen_private_key.sh
@@ -1,2 +1,2 @@
 # Generate private key and self-signed certificate
-openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365 -subj "/CN=localhost"
+openssl req -x509 -newkey rsa:4096 -nodes -out ssl/cert.pem -keyout ssl/key.pem -days 365 -subj "/CN=localhost"
